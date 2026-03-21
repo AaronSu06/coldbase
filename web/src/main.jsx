8b3025a36a2b107565f28c80d2d70230454e77fd
Aaron Su <aaronzsu09@gmail.com>
2026-03-20 20:14:22 -0400

    feat(web): add BrowserRouter with PrivateRoute protecting the main app

--- web/src/main.jsx
+++ web/src/main.jsx
@@ -1,10 +1,25 @@
+// web/src/main.jsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import App from './App';
+import LoginPage from './components/LoginPage';
+import SignupPage from './components/SignupPage';
 import './index.css';
+
+function PrivateRoute({ children }) {
+  const token = localStorage.getItem('reach_token');
+  return token ? children : <Navigate to="/login" replace />;
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/*" element={<PrivateRoute><App /></PrivateRoute>} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
